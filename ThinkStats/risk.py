9e47252ecadbe909026416a3cc0b0b32fa03f47c
Aleksey Kulikov <Aleksey_Kul@abbyy.com>
2018-03-30 17:25:21 +0300

    conditional probability chapter 2

--- ThinkStats/risk.py
+++ ThinkStats/risk.py
@@ -14,6 +14,11 @@ def prob_birth(lst):
 	total = num_early + num_on_time + num_late
 	return num_early / total, num_on_time / total, num_late / total
 
+def prob_next_week(lst, weeknum):
+	num_total = max(sum([i for i in lst if int(i) >= weeknum]), 1)
+	num_this_week = sum([i for i in lst if int(i) == weeknum])
+	return num_this_week / num_total
+
 if __name__ == '__main__':
 	table = survey.Pregnancies()
 	table.ReadRecords('./data')
@@ -30,3 +35,7 @@ if __name__ == '__main__':
 	print('{0} (first births)'.format(prob_first))
 	print('{0} (other births)'.format(prob_other))
 	print('{0} (first / other: relative risk)'.format(prob_first / prob_other))
+
+	print('Probabilities of being born next week (first babies, other babies, all babies):')
+	for week in range(35, 50):
+		print('{3}\t{0}\t{1}\t{2}'.format(prob_next_week(df_first['prglength'], week), prob_next_week(df_other['prglength'], week), prob_next_week(df_all_live['prglength'], week), week))
